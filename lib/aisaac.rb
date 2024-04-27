cdaa5b31d781504bb86b80917e8311296ac1bc3d
gogotanaka <mail@tanakakazuki.com>
2024-04-27 16:20:36 +0900

    first commit

---
+++ lib/aisaac.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "aisaac/version"
+
+module Aisaac
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/aisaac_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Aisaac do
+  it "has a version number" do
+    expect(Aisaac::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
